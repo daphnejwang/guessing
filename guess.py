7c98b24fbf54b94bd6ee47685ad184dfca146384
Hackbright Student <info@hackbrightacademy.com>
2014-06-03 17:08:59 -0700

    Changed num variable name to target_num

--- guess.py
+++ guess.py
@@ -10,19 +10,19 @@ def get_guess():
 	    print "Please enter an integer."
 
 def main():
-	num = random.randint(1,100)
+	target_num = random.randint(1,100)
 	num_guesses = 1
 	
 	print "Howdy, what's your name?"
 	name = raw_input("(type in your name) ")
 	print "%s, I'm thinking of a number between 1 and 100. Try to guess it" % name
-	print num
+	print target_num
 
 	guess = get_guess()
 	
-	while guess != num:
+	while guess != target_num:
 		num_guesses += 1
-		if guess > num:
+		if guess > target_num:
 			print "Your guess is too high! Try again."
 		else:
 			print "Your guess is too low! Try again."
